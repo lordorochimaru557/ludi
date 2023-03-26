35d9689b0d7c89ab41e4959be2653b9bbb7bc4bf
lordorochimaru557 <aesquer.caraveo@itvy.edu.mx>
2023-03-26 15:19:52 -0700

    Primera version de ludi.com
    Contiene solamente la barra de navegacion y una imagen

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'ludi':{
+          DEFAULT: '#016aa3',
+          50: '#f0f9ff',
+          100:'#e0f2fe',
+          200:'#b9e6fe',
+          300:'#7cd4fd',
+          400:'#36bffa',
+          500:'#0ca8eb',
+          600:'#0080c0',
+          700:'#016aa3',
+          800:'#065a86',
+          900:'#0b4b6f'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
